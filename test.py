381e6c2fcc8de7ca8047bbadb1f1273f7ca15a8a
Poojitha <poojitha200624@gmail.com>
2026-07-05 19:02:00 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from login import login
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.maximize_window()
+
+driver.get("https://tichi-app-webapp-stage.web.app/login")
+
+login(driver, "poojitha200624@gmail.com", "Pooja@24")
+
+print("Valid Login Test Executed Successfully")
+
+input("Press Enter to close...")
+driver.quit()
